de0a12bbd596b40bc5e1c18f8e9e50f37cb1e1e3
Nick Winters <nwinters3000@gmail.com>
2014-04-11 18:15:16 -0700

    Allow configuring the machine size

--- src/modules/robot/arm_solutions/XPlotterSolution.cpp
+++ src/modules/robot/arm_solutions/XPlotterSolution.cpp
@@ -5,10 +5,20 @@
 // #define cartesian_height ((23.0f-2.25)*25.4f)
 // #define cartesian_depth 0.0f
 
-#define cartesian_width  (610.0f-60.0f)
-#define cartesian_height (612.0f-60.0f)
-#define cartesian_depth 0.0f
+// smaller
+//#define cartesian_width  (610.0f-60.0f)
+//#define cartesian_height (612.0f-60.0f)
+//#define cartesian_depth 0.0f
 
+
+XPlotterSolution::XPlotterSolution(Config* config)
+{
+	// machine size
+	// Should count the "moving" size = width of machine minus width of carriage.
+    cartesian_width         = config->value(cartesian_width_checksum)->by_default((610.0f-60.0f))->as_number();
+    cartesian_height        = config->value(cartesian_height_checksum)->by_default((612.0f-60.0f))->as_number();
+    cartesian_depth         = config->value(cartesian_depth_checksum)->by_default(0.0f)->as_number();
+}
 
 // magnitude of 2d vector (x,y)
 float mag2d(float x, float y) {
@@ -22,7 +32,7 @@ void XPlotterSolution::cartesian_to_actuator( float cartesian_mm[], float actuat
 
 	// the string length of all strings when the carriage is at the origin:
 	float base_distance = 
-		mag2d(.5f*(cartesian_width), .5f*(cartesian_height)/*, cartesian_depth*/);
+		mag2d(.5f*(this->cartesian_width), .5f*(this->cartesian_height)/*, this->cartesian_depth*/);
 		//0.0f;
 	
 	float x = cartesian_mm[X_AXIS];
@@ -31,13 +41,13 @@ void XPlotterSolution::cartesian_to_actuator( float cartesian_mm[], float actuat
 
 	
 	// a (top left)
-	actuator_mm[ALPHA_STEPPER] = -base_distance+mag2d(cartesian_width*.5f+x,cartesian_height*.5f-y);
+	actuator_mm[ALPHA_STEPPER] = -base_distance+mag2d(this->cartesian_width*.5f+x,this->cartesian_height*.5f-y);
 	// b (top right)
-	actuator_mm[BETA_STEPPER ] = -base_distance+mag2d(cartesian_width*.5f-x,cartesian_height*.5f-y);
+	actuator_mm[BETA_STEPPER ] = -base_distance+mag2d(this->cartesian_width*.5f-x,this->cartesian_height*.5f-y);
 	// c (bottom right)
-	actuator_mm[GAMMA_STEPPER] = -base_distance+mag2d(cartesian_width*.5f-x,cartesian_height*.5f+y);
+	actuator_mm[GAMMA_STEPPER] = -base_distance+mag2d(this->cartesian_width*.5f-x,this->cartesian_height*.5f+y);
 	// d (bottom left)
-	actuator_mm[DELTA_STEPPER] = -base_distance+mag2d(cartesian_width*.5f+x,cartesian_height*.5f+y);
+	actuator_mm[DELTA_STEPPER] = -base_distance+mag2d(this->cartesian_width*.5f+x,this->cartesian_height*.5f+y);
 }
 
 void XPlotterSolution::actuator_to_cartesian( float actuator_mm[], float cartesian_mm[] ){
--- src/modules/robot/arm_solutions/XPlotterSolution.h
+++ src/modules/robot/arm_solutions/XPlotterSolution.h
@@ -7,12 +7,20 @@
 
 #include "libs/Config.h"
 
+#define cartesian_width_checksum         CHECKSUM("zar_cartesian_width")
+#define cartesian_height_checksum         CHECKSUM("zar_cartesian_height")
+#define cartesian_depth_checksum         CHECKSUM("zar_cartesian_depth")
+
 class XPlotterSolution : public BaseSolution {
     public:
         XPlotterSolution();
-        XPlotterSolution(Config*){};
+        XPlotterSolution(Config*);
         void cartesian_to_actuator( float[], float[] );
         void actuator_to_cartesian( float[], float[] );
+    private:
+        float cartesian_width;
+        float cartesian_height;
+        float cartesian_depth;
 };
 
 #endif // XPLOTTERSOLUTION_H
